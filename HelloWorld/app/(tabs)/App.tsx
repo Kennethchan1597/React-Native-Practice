d0c824147f9e4a2f9c18ca793dc5e78d61f812e7
ashimeru <hkchan229@gmail.com>
2025-10-04 20:23:37 +0800

    update

--- HelloWorld/app/(tabs)/App.tsx
+++ HelloWorld/app/(tabs)/App.tsx
@@ -4,14 +4,13 @@ import logoImg from "../../assets/images/icon.png";
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 80 }}>
-            <ImageBackground source={logoImg} style={{flex:1}}>
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
         <Text>Image Text</Text>
       </ImageBackground>
       <Text>
-        <Text style={{color: "white"}}>Hello </Text>Wosrld
+        <Text style={{ color: "white" }}>Hello </Text>Wosrld
       </Text>
-      <Image source={logoImg} style={{width:300, height:300}}></Image>
-      <Image source={{uri:'https://picsum.photos/300'}} style={{width:300, height:300}}></Image>
+      <Image source={logoImg} style={{ width: 300, height: 300 }}></Image>
     </View>
   );
 }
