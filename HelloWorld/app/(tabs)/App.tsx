82214a3c0cd826fc5b0666ea9a747b2330b9882c
ashimeru <hkchan229@gmail.com>
2025-10-04 20:47:37 +0800

    Toggle button added

--- HelloWorld/app/(tabs)/App.tsx
+++ HelloWorld/app/(tabs)/App.tsx
@@ -1,19 +1,22 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Image, ScrollView, Text, View } from "react-native";
 import logoImg from "../../assets/images/icon.png";
 
 export default function App() {
+
+  const [isImageVisible, setImageVisible] = useState(true);
+
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 80 }}>
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Image Text</Text>
-      </ImageBackground>
-      <Text>
-        <Text style={{ color: "white" }}>Hello </Text>Wosrld
-      </Text>
-      <Image source={logoImg} style={{ width: 300, height: 300 }}></Image>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }}></Image>
+        <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat a esse quae ipsam eveniet delectus hic, sunt omnis? Dolorem laborum minus at nam atque dolore unde corporis, quo repellendus quisquam?</Text>
+        <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat a esse quae ipsam eveniet delectus hic, sunt omnis? Dolorem laborum minus at nam atque dolore unde corporis, quo repellendus quisquam?</Text>
+        <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat a esse quae ipsam eveniet delectus hic, sunt omnis? Dolorem laborum minus at nam atque dolore unde corporis, quo repellendus quisquam?</Text>
+        <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat a esse quae ipsam eveniet delectus hic, sunt omnis? Dolorem laborum minus at nam atque dolore unde corporis, quo repellendus quisquam?</Text>
+        <Button title={isImageVisible ? "Hide Image" : "Show Image"} onPress={() => setImageVisible(!isImageVisible)} />
+        {isImageVisible && <Image source={logoImg} style={{ width: 300, height: 300 }}></Image>}
+      </ScrollView>
     </View>
   );
 }
-
-// Move this declaration to a separate file named `declarations.d.ts` in the root of your project.
-
