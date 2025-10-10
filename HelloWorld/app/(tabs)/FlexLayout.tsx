ca98d95b11400a3a5d056aa712d4ee6238c24b2b
ashimeru <hkchan229@gmail.com>
2025-10-10 20:38:37 +0800

    update

--- HelloWorld/app/(tabs)/FlexLayout.tsx
+++ HelloWorld/app/(tabs)/FlexLayout.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from "react-native";
 export default function FlexLayout() {
   return (
     <View style={styles.container}>
-      <Box style={{color:"white", flexBasis:10000}} name={"Box1"}/>
+      <Box style={{color:"white"}} name={"Box1"}/>
       <Box style={{color:"white"}} name={"Box2"}/>
       <Box style={{color:"white"}} name={"Box3"}/>
       <Box style={{color:"white"}} name={"Box4"}/>
