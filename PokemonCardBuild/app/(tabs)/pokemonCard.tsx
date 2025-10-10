c96f3be81af0b17b8c7bc398003cb8cdbf278a13
ashimeru <hkchan229@gmail.com>
2025-10-11 05:05:49 +0800

    update

---
+++ PokemonCardBuild/app/(tabs)/pokemonCard.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from "react-native";
+
+export function pokemonCard() {
+  return (
+    <Text>Hello</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex:1, backgroundColor: "#f5f5f5"}
+});
