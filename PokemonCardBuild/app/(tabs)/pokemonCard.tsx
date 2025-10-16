a537951f914e71d13b5d0ce8a87bad261d3773f5
ashimeru <hkchan229@gmail.com>
2025-10-16 21:54:05 +0800

    update

--- PokemonCardBuild/app/(tabs)/pokemonCard.tsx
+++ PokemonCardBuild/app/(tabs)/pokemonCard.tsx
@@ -1,11 +1,25 @@
-import { StyleSheet, Text } from "react-native";
+import PokemonCard from "@/components/PokemonCard";
+import { Platform, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export function pokemonCard() {
+export default function pokemonCard() {
+
+  const charmanderData = {
+    name: "Charmander",
+    image: require("../../assets/images/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["water", "Rock"]
+  }
+
   return (
-    <Text>Hello</Text>
+    <SafeAreaView style={styles.container}>
+        <PokemonCard {...charmanderData} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex:1, backgroundColor: "#f5f5f5"}
+  container: { flex: 1, backgroundColor: "#f5f5f5", paddingTop: Platform.OS === "android" ? 25 : 0 }
 });
